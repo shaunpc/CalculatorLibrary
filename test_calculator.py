6a3393666a88b7c801efa9c6c32ec9c4ced87794
shaunpc <shauncotter00@gmail.com>
2018-12-02 13:09:49 +0000

    added mult function and tests

--- test_calculator.py
+++ test_calculator.py
@@ -8,11 +8,23 @@ import calculator
 
 class TestCalculator(unittest.TestCase):
 
-    def test_addition(self):
+    def test_addition1(self):
         self.assertEqual(4, calculator.add(2, 2))
 
-    def test_subtraction(self):
+    def test_addition2(self):
+        self.assertNotEqual(6, calculator.add(6, 2))
+
+    def test_subtraction1(self):
         self.assertEqual(2, calculator.subtract(4, 2))
+
+    def test_subtraction2(self):
+        self.assertNotEqual(3, calculator.subtract(8, 3))
+
+    def test_multiplication1(self):
+        self.assertEqual(12, calculator.multiply(2, 6))
+
+    def test_multiplication2(self):
+        self.assertNotEqual(22, calculator.multiply(3, 6))
 
 
 if __name__ == '__main__':
