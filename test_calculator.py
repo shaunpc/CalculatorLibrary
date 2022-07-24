77292b07ae55a62a222c540d8204defbf91f7504
shaun <shauncotter00@gmail.com>
2022-07-24 13:18:11 +0100

    Minor updates to test functions

--- test_calculator.py
+++ test_calculator.py
@@ -1,9 +1,11 @@
-import unittest
-import calculator
-
 """
     Unit tests for the calculator library
+    
+    NOTE: 
+    Project Run configuration has target module: test_calculator.TestCalculator
 """
+import unittest
+import calculator
 
 
 class TestCalculator(unittest.TestCase):
@@ -13,6 +15,12 @@ class TestCalculator(unittest.TestCase):
 
     def test_addition2(self):
         self.assertNotEqual(6, calculator.add(6, 2))
+
+    def test_addition3(self):
+        self.assertNotEqual(9, calculator.add(6, 2))
+
+    def test_addition4(self):
+        self.assertEqual(8, calculator.add(6, 2))
 
     def test_subtraction1(self):
         self.assertEqual(2, calculator.subtract(4, 2))
@@ -26,7 +34,12 @@ class TestCalculator(unittest.TestCase):
     def test_multiplication2(self):
         self.assertNotEqual(22, calculator.multiply(3, 6))
 
+    def test_divide1(self):
+        self.assertEqual(2, calculator.divide(4, 2))
+
+    def test_divide2(self):
+        self.assertNotEqual(3, calculator.divide(8, 3))
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
-
